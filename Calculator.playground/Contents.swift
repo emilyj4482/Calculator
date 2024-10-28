1be7c4aef2f917163f1c45cd33ed5134544a7ee6
emilyj4482 <emilyj4482@gmail.com>
2024-10-28 15:24:14 +0900

    [playground] 필수 기능 Lv1 구현 완료

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,12 +1,42 @@
 import Foundation
 
 class Calculator {
-    // TODO: 내부 구현하기
-    func calculate(firstNumber: Double, secondNumber: Double, operation: Operation) -> Double {
-        return 0.0
+    // 외부에서 호출할 함수
+    // 파라미터 : 연산 종류, 연산 대상 수 2개
+    // 반환 : 연산 결과
+    func calculate(operation: Operation, firstNumber: Double, secondNumber: Double) -> Double {
+        switch operation {
+        case .add:
+            return add(firstNumber, secondNumber)
+        case .substract:
+            return substract(firstNumber, secondNumber)
+        case .multiply:
+            return multiply(firstNumber, secondNumber)
+        case .divide:
+            return divide(firstNumber, secondNumber)
+        }
+
+    }
+    
+    // 사칙연산 수행 함수
+    private func add(_ a: Double, _ b: Double) -> Double {
+        return a + b
+    }
+    
+    private func substract(_ a: Double, _ b: Double) -> Double {
+        return a - b
+    }
+    
+    private func multiply(_ a: Double, _ b: Double) -> Double {
+        return a * b
+    }
+    
+    private func divide(_ a: Double, _ b: Double) -> Double {
+        return a / b
     }
 }
 
+// 연산 종류 enum
 enum Operation {
     case add
     case substract
@@ -17,7 +47,8 @@ enum Operation {
 // 인스턴스 생성하여 변수에 할당
 let calculator = Calculator()
 
-// TODO: calculator 변수를 활용하여 사칙연산을 진행
-let result = calculator.calculate(firstNumber: 1, secondNumber: 2, operation: .add)
-
-print(result)
+// calculator 변수를 활용하여 사칙연산 진행
+let addResult = calculator.calculate(operation: .add, firstNumber: 1, secondNumber: 2)
+let substractResult = calculator.calculate(operation: .substract, firstNumber: 1, secondNumber: 2)
+let multiplyResult = calculator.calculate(operation: .multiply, firstNumber: 1, secondNumber: 2)
+let divideResult = calculator.calculate(operation: .divide, firstNumber: 1, secondNumber: 2)
