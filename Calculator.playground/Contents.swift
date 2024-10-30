f3c7967324cd414e28de7b69a17a9359667da509
emilyj4482 <emilyj4482@gmail.com>
2024-10-30 14:33:30 +0900

    [playground] protocol 적용하여 답안 수정

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -2,31 +2,41 @@ import Foundation
 
 class Calculator {
     
-    // 사칙 연산 클래스의 인스턴스
-    let addOperation = AddOperation()
-    let substractOperation = SubstractOperation()
-    let multiplyOperation = MultiplyOperation()
-    let divideOperation = DivideOperation()
+    // 연산 인스턴스
+    let operation: AbstractOperation
     
+    // 계산 할 수 프로퍼티
+    let firstNumber: Double
+    let secondNumber: Double
+    
+    init(operation: AbstractOperation, firstNumber: Double, secondNumber: Double) {
+        self.operation = operation
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+    }
+    
+    func calculate() -> Double {
+        operation.calculate(firstNumber, secondNumber)
+    }
+    
+    /*
     // 외부에서 호출할 함수
-    // 파라미터 : 연산 종류, 연산 대상 수 2개
-    // 반환 : 연산 결과
     func calculate(operation: Operation, firstNumber: Double, secondNumber: Double) -> Double {
         switch operation {
         case .add:
-            return addOperation.add(firstNumber, secondNumber)
+            return addOperation.calculate(firstNumber, secondNumber)
         case .substract:
-            return substractOperation.substract(firstNumber, secondNumber)
+            return substractOperation.calculate(firstNumber, secondNumber)
         case .multiply:
-            return multiplyOperation.multiply(firstNumber, secondNumber)
+            return multiplyOperation.calculate(firstNumber, secondNumber)
         case .divide:
-            return divideOperation.divide(firstNumber, secondNumber)
+            return divideOperation.calculate(firstNumber, secondNumber)
         }
-
-    }
+     }
+     */
     
-    // 사칙연산 수행 함수
     /*
+    // 사칙연산 수행 함수
     private func add(_ a: Double, _ b: Double) -> Double {
         return a + b
     }
@@ -45,12 +55,15 @@ class Calculator {
     */
     
     // 나머지 연산
-    func remainder(firstNumber a: Int, secondNumber b: Int) -> Int {
-        return a % b
+    /*
+    func remainder(firstNumber a: Double, secondNumber b: Double) -> Double {
+        // return a % b
+        return a.truncatingRemainder(dividingBy: b)
     }
+    */
 }
 
-// 연산 종류 enum
+// 연산 종류 enum (protocol을 사용하도록 코드를 수정하면서 쓰지 않게 됨)
 enum Operation {
     case add
     case substract
@@ -58,30 +71,46 @@ enum Operation {
     case divide
 }
 
-class AddOperation {
-    func add(_ a: Double, _ b: Double) -> Double {
+protocol AbstractOperation {
+    func calculate(_: Double, _: Double) -> Double
+}
+
+class AddOperation: AbstractOperation {
+    func calculate(_ a: Double, _ b: Double) -> Double {
         return a + b
     }
 }
 
-class SubstractOperation {
-    func substract(_ a: Double, _ b: Double) -> Double {
+class SubstractOperation: AbstractOperation {
+    func calculate(_ a: Double, _ b: Double) -> Double {
         return a - b
     }
 }
 
-class MultiplyOperation {
-    func multiply(_ a: Double, _ b: Double) -> Double {
+class MultiplyOperation: AbstractOperation {
+    func calculate(_ a: Double, _ b: Double) -> Double {
         return a * b
     }
 }
 
-class DivideOperation {
-    func divide(_ a: Double, _ b: Double) -> Double {
-        return a / b
+class DivideOperation: AbstractOperation {
+    func calculate(_ a: Double, _ b: Double) -> Double {
+        if b == 0 {
+            print("[ERROR] You cannot divide by zero.")
+            return 0
+        } else {
+            return a / b
+        }
     }
 }
 
+class RemainderOperation: AbstractOperation {
+    func calculate(_ a: Double, _ b: Double) -> Double {
+        return a.truncatingRemainder(dividingBy: b)
+    }
+}
+
+/*
 // 인스턴스 생성하여 변수에 할당
 let calculator = Calculator()
 
@@ -89,5 +118,18 @@ let calculator = Calculator()
 let addResult = calculator.calculate(operation: .add, firstNumber: 1, secondNumber: 2)
 let substractResult = calculator.calculate(operation: .substract, firstNumber: 1, secondNumber: 2)
 let multiplyResult = calculator.calculate(operation: .multiply, firstNumber: 1, secondNumber: 2)
-let divideResult = calculator.calculate(operation: .divide, firstNumber: 1, secondNumber: 2)
-let remainderResult = calculator.remainder(firstNumber: 1, secondNumber: 2)
+let divideResult = calculator.calculate(operation: .divide, firstNumber: 1, secondNumber: 0)
+let remainderResult = calculator.remainder(firstNumber: 10, secondNumber: 2)
+*/
+
+let addResult = Calculator(operation: AddOperation(), firstNumber: 1, secondNumber: 2)
+let substractResult = Calculator(operation: SubstractOperation(), firstNumber: 1, secondNumber: 2)
+let multiplyResult = Calculator(operation: MultiplyOperation(), firstNumber: 1, secondNumber: 2)
+let divideResult = Calculator(operation: DivideOperation(), firstNumber: 10, secondNumber: 0)
+let remainderResult = Calculator(operation: RemainderOperation(), firstNumber: 10, secondNumber: 3)
+
+addResult.calculate()
+substractResult.calculate()
+multiplyResult.calculate()
+divideResult.calculate()
+remainderResult.calculate()
