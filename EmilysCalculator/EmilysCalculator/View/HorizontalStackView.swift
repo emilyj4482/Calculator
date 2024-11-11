4af799fb0049b36899d2aaa82a0449ca8e2bdca1
emilyj4482 <emilyj4482@gmail.com>
2024-11-11 15:19:56 +0900

    [assignment/new] horizontal stack view 추가

--- EmilysCalculator/EmilysCalculator/View/HorizontalStackView.swift
+++ EmilysCalculator/EmilysCalculator/View/HorizontalStackView.swift
@@ -5,4 +5,30 @@
 //  Created by EMILY on 11/11/2024.
 //
 
-import Foundation
+import UIKit
+
+class HorizontalStackView: UIStackView {
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        axis = .horizontal
+        spacing = 10
+        distribution = .fillEqually
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: 80).isActive = true
+    }
+    
+    func addSubviews(_ buttons: [Button]) {
+        buttons.forEach {
+            addArrangedSubview($0)
+        }
+    }
+}
