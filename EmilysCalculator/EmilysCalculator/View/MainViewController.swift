b541fc90b3e67c09065ec362da0904641bd509ee
emilyj4482 <emilyj4482@gmail.com>
2024-11-20 22:11:45 +0900

    [assignment/feat] label에 scrollview 적용

--- EmilysCalculator/EmilysCalculator/View/MainViewController.swift
+++ EmilysCalculator/EmilysCalculator/View/MainViewController.swift
@@ -14,6 +14,16 @@ class MainViewController: UIViewController {
     
     private let buttonTapService = ButtonTapService.shared
     
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        
+        view.backgroundColor = .black
+        view.indicatorStyle = .white
+        view.contentAlignmentPoint.x = 1
+        
+        return view
+    }()
+    
     private lazy var inputLabel: UILabel = {
         let label = UILabel()
         
@@ -34,12 +44,18 @@ class MainViewController: UIViewController {
         bind()
     }
     
+    override func viewDidLayoutSubviews() {
+        setScrollView()
+    }
+    
     private func addSubview() {
-        [inputLabel, buttonView]
+        [scrollView, buttonView]
             .forEach {
                 view.addSubview($0)
                 $0.translatesAutoresizingMaskIntoConstraints = false
             }
+        scrollView.addSubview(inputLabel)
+        inputLabel.translatesAutoresizingMaskIntoConstraints = false
     }
     
     private func layout() {
@@ -49,15 +65,30 @@ class MainViewController: UIViewController {
         let offset: CGFloat = 30.0
         
         NSLayoutConstraint.activate([
-            inputLabel.leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: offset),
-            inputLabel.trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -offset),
-            inputLabel.topAnchor.constraint(equalTo: superView.topAnchor, constant: 200),
-            inputLabel.heightAnchor.constraint(equalToConstant: 100),
+            scrollView.leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: offset),
+            scrollView.trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -offset),
+            scrollView.topAnchor.constraint(equalTo: superView.topAnchor, constant: 200),
+            scrollView.heightAnchor.constraint(equalToConstant: 100),
             
-            buttonView.topAnchor.constraint(equalTo: inputLabel.bottomAnchor, constant: 60),
+            inputLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            inputLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            inputLabel.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            inputLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            inputLabel.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+            
+            buttonView.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 60),
             buttonView.centerXAnchor.constraint(equalTo: superView.centerXAnchor),
             buttonView.widthAnchor.constraint(equalToConstant: 350)
         ])
+    }
+    
+    private func setScrollView() {
+        // 스크롤 뷰의 content size를 label size와 일치시킴
+        scrollView.contentSize = CGSize(width: inputLabel.intrinsicContentSize.width, height: inputLabel.bounds.height)
+        
+        // 스크롤을 우측에 고정
+        let rightOffset = CGPoint(x: scrollView.contentSize.width - scrollView.bounds.width, y: 0)
+        scrollView.setContentOffset(rightOffset, animated: false)
     }
     
     private func bind() {
